27938bc98971e73156c815216eb94e6a76a521c9
omkar pathade <omkar.pathade2011@gmail.com>
2021-12-29 17:02:39 +0530

    Initial Project

---
+++ src/main/java/com/example/persondata/PersondataApplication.java
@@ -0,0 +1,13 @@
+package com.example.persondata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersondataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersondataApplication.class, args);
+	}
+
+}
