7ae67ede550ee80e6fa26614d29b4c04680e25ce
omkar pathade <omkar.pathade2011@gmail.com>
2021-12-29 15:52:22 +0530

    Added Repository

---
+++ src/main/java/com/example/firstjava/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.example.firstjava.repository;
+
+import com.example.firstjava.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author,Long> {
+}
